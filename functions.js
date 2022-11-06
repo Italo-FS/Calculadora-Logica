b89c9546b080020dce402fed84480a674078e4bb
Italo Fontenele <italo.fontenele.silva@gmail.com>
2022-11-06 14:52:15 -0300

    Melhoria na tabela verdade
    Melhoria na tabela verdade e mais refatoração de código

--- functions.js
+++ functions.js
@@ -1,8 +1,7 @@
 expression = ""
 
-// let textBox = document.getElementById('message');
 document.addEventListener('keydown', (event) => {
-	console.log(`key=${event.key},code=${event.code}`);
+	// console.log(`key=${event.key},code=${event.code}`);
 
 	if (event.key.length === 1 && ord(event.key)>64 && ord(event.key)<91) {
 		inserir(event.key)
@@ -31,7 +30,7 @@ document.addEventListener('keydown', (event) => {
 				inserir('[Falso]')
 				break
 			default:
-				console.log(`Tecla [key=${event.key},code=${event.code}] não configurada`)
+				// console.log(`Tecla [key=${event.key},code=${event.code}] não configurada`)
 		}
 	}
 });
@@ -44,19 +43,58 @@ function chr(code){
 	return String.fromCharCode(code)
 }
 
-// function addColumn () {
-// 	header = "header"
+function checa_se_repetido(str, array) {
+	for (let v of array) {
+		if (str === v) return true
+	}
+	return false
+}
 
-// 	document.querySelectorAll('#tabela_resultado tr').forEach((row, i) => {
-//         const cell = document.createElement(i ? "td" : "th")
-// 		cell.innerHTML = (i ? i : header)
+function estruturar_tabela_resposta(obj, qtde_linhas) {
+	limpar_resultado() //Limpa os resultados anteriores
 
-//         // cell.appendChild(input)
-//         row.appendChild(cell)
-//     })
-// }
+	let table = document.createElement("table") //Cria uma nova tabela que exibirá os resultados
+	table.classList.add('table')
+	table.id = "tabela_resultado"
+	table.style = "margin-top: 2em;"
 
-var addBinary = function(a, b) {
+	let thead = table.createTHead() //Cria o cabeçalho da tabela	
+
+	{ //Título da tabela
+		let row = thead.insertRow() //Cria a linha do cabeçalho
+		let th = document.createElement("th");
+		th.innerHTML = "Tabela Verdade"
+		th.colSpan = Object.keys(obj).length
+		row.appendChild(th)
+	}
+
+	let row = thead.insertRow() //Cria a linha do cabeçalho
+
+	for (let header in obj) { //Preenche o cabeçalho		
+		let th = document.createElement("th");
+		th.innerHTML = header
+		row.appendChild(th)
+	}
+
+	table.appendChild(thead)
+
+	let tbody = table.createTBody()
+
+	for (let i=0; i<qtde_linhas; i++) {
+		let row = tbody.insertRow()
+		for (let header in obj) {
+			let td = document.createElement("td");
+			td.innerHTML = obj[header][i]
+			row.appendChild(td)
+		}
+	}
+
+	table.appendChild(tbody)
+
+	document.querySelector("#resultado").appendChild(table)
+}
+
+function addBinary(a, b) {
     var i = a.length - 1;
     var j = b.length - 1;
     var carry = 0;
@@ -82,8 +120,17 @@ function inserir(str) {
 }
 
 function limpar() {
-	expression = ""
-	atualizar_expressao()
+	if (expression != "") {
+		expression = ""
+		atualizar_expressao()
+	}
+	else {
+		limpar_resultado()
+	}
+}
+
+function limpar_resultado(){
+	document.querySelector("#resultado").innerHTML = ""
 }
 
 function backspace() {
@@ -92,7 +139,7 @@ function backspace() {
 }
 
 function calcular() {
-	estruturar_tabela_resposta()
+	estruturar_resposta()
 	limpar()
 }
 
@@ -122,76 +169,121 @@ function ordenar_variaveis(texto) {
 	)
 }
 
-function estruturar_tabela_resposta() {
+function estruturar_resposta() {
 	expression = corrigir_expressao(expression)
 	let variaveis = ordenar_variaveis(expression)
-	
-
-	let qtde_linhas = (2**variaveis.length)
-
-	document.querySelector("#resultado").innerHTML = ""
-
-	let table = document.createElement("table")
-
-	//Estruturação do cabeçalho da tabela
-	let thead = table.createTHead()
-	let row = thead.insertRow()
-	for (let coluna of variaveis) {
-		let th = document.createElement("th");
-		th.innerHTML = coluna
-		row.appendChild(th)
-	}
-
-	let th = document.createElement("th");
-	th.innerHTML = expression
-	row.appendChild(th)
-
-	thead.appendChild(row)
+	let qtde_linhas_tabela = (2**variaveis.length)
 	let bin = "0".repeat(variaveis.length)
+	let array_tabela_resultado = {}
 
-	//Estruturação do corpo da tabela
-	let tbody = table.createTBody()
-	for (linha = 0; linha<qtde_linhas; linha++) {
-		let row = table.insertRow()
-		let valores = {} //Valores atribuídos as variáveis que serão testadas em cada linha da tabela
-		for (let i = 0; i<variaveis.length; i++) {
-			let td = document.createElement("td");
-			td.innerHTML = (bin[i] === '0') ? "V" : "F"
-			valores[variaveis[i]] = (bin[i] === '0') ? "1" : "0"
+	for (let i = 0; i<qtde_linhas_tabela; i++) {
+		let valores = {}		
+		for (let j = 0; j<variaveis.length; j++) {
+			valores[variaveis[j]] = (bin[j] === '0') ? "1" : "0"
 
-			row.appendChild(td)
+			if (typeof array_tabela_resultado[variaveis[j]] === "undefined") {
+				array_tabela_resultado[variaveis[j]] = []
+			}
+			array_tabela_resultado[variaveis[j]].push((bin[j] === '0') ? "V" : "F")
 		}
 
-		let td = document.createElement("td");
-		td.innerHTML = calcular_expressao(expression, valores) === '1' ? "Verdadeiro" : "Falso"
-		row.appendChild(td)
+		resposta = calcular_expressao(expression, valores)
+		for (let expressao of resposta[1].split('|')) {
+			let exp = expressao.split(':')
+			if (exp[0] != "" && !checa_se_repetido(exp[0], variaveis)) {
+				if (typeof array_tabela_resultado[exp[0]] === "undefined") {
+					array_tabela_resultado[exp[0]] = []
+				}
+				array_tabela_resultado[exp[0]].push((exp[1]=== '1') ? "V" : "F")
+			}
 
-		tbody.appendChild(row)
+		}
+		// console.log(calcular_expressao(expression, valores)[1].split('|'))
 		bin = addBinary(bin,"1")
 	}
 
-	table.appendChild(thead)
-	table.appendChild(tbody)
-	table.classList.add('table')
-	table.id = "tabela_resultado"
-	document.querySelector("#resultado").appendChild(table)
+	estruturar_tabela_resposta(array_tabela_resultado, qtde_linhas_tabela)
+
+	// console.log(array_tabela_resultado)
+	
+
+
+
+
+
+
+
+
+
+
+	
+	return
+	
+
+	// document.querySelector("#resultado").innerHTML = ""
+
+	// let table = document.createElement("table")
+
+	// //Estruturação do cabeçalho da tabela
+	// let thead = table.createTHead()
+	// let row = thead.insertRow()
+	// for (let coluna of variaveis) {
+	// 	let th = document.createElement("th");
+	// 	th.innerHTML = coluna
+	// 	row.appendChild(th)
+	// }
+
+	// let th = document.createElement("th");
+	// th.innerHTML = expression
+	// row.appendChild(th)
+
+	// thead.appendChild(row)
+	// let bin = "0".repeat(variaveis.length)
+
+	// //Estruturação do corpo da tabela
+	// let tbody = table.createTBody()
+	// for (linha = 0; linha<qtde_linhas; linha++) {
+	// 	let row = table.insertRow()
+	// 	let valores = {} //Valores atribuídos as variáveis que serão testadas em cada linha da tabela
+	// 	for (let i = 0; i<variaveis.length; i++) {
+	// 		let td = document.createElement("td");
+	// 		td.innerHTML = (bin[i] === '0') ? "V" : "F"
+	// 		valores[variaveis[i]] = (bin[i] === '0') ? "1" : "0"
+
+	// 		row.appendChild(td)
+	// 	}
+
+	// 	let td = document.createElement("td");
+	// 	td.innerHTML = calcular_expressao(expression, valores) === '1' ? "Verdadeiro" : "Falso"
+	// 	row.appendChild(td)
+
+	// 	tbody.appendChild(row)
+	// 	bin = addBinary(bin,"1")
+	// }
+
+	// table.appendChild(thead)
+	// table.appendChild(tbody)
+	// table.classList.add('table')
+	// table.id = "tabela_resultado"
+	// document.querySelector("#resultado").appendChild(table)
 }
 
 function atualizar_expressao() {
 	document.querySelector("#expressao").innerHTML = expression
 }
 
-function calcular_expressao(str, obj) {
+function calcular_expressao(str, obj, string_result="") {
 	result = str.replaceAll("[Verdadeiro]","1").replaceAll("[Falso]","0")
-	console.log(result)
-
-	for (let variable in obj) {
-		result = result.replace(variable, obj[variable])
-	}
 
 	result = result.replaceAll(/\(([^\(\)]*)\)/g, (match, p) => { //Teste do Parenteses
-		return calcular_expressao(p)
+		response = calcular_expressao(p, obj, string_result)
+		string_result = `${string_result}${response[1]}`
+		return response[0]
 	})
+
+	for (let variable in obj) { //Troca variaveis por seus valores
+		result = result.replaceAll(variable, obj[variable])
+	}
 
 	result = result.replaceAll(/∼(0|1)/g, (match, p) => { //Teste do 'NÃO p'
 		return (p==='0') ? '1' : '0'
@@ -213,7 +305,8 @@ function calcular_expressao(str, obj) {
 		return ((p==='1' && q==='1') || (p==='0' && q==='0')) ? '1' : '0'
 	})
 
-	return result
+	string_result = `${string_result}|${str}:${result}`
+	return [result, string_result]
 }
 
 limpar()
